547443362862fe3e338c47da54332d9735fc536a
Yugansh1234 <yukjain1984@gmail.com>
2026-01-06 16:03:57 +0530

    Add files via upload

---
+++ 2ndactivity.py
@@ -0,0 +1,6 @@
+name = input("enter your name:")
+print("welcome to codingal: " ,name)
+age = input("what is your age:")
+print("your age is:" ,age)
+print("my name is " ,name, " my age is " ,age)
+
