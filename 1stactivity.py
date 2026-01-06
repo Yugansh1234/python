68364d9fce20b1a551f7c8909bd3bf5ef47959dd
Yugansh1234 <yukjain1984@gmail.com>
2026-01-06 16:04:20 +0530

    Add files via upload

---
+++ 1stactivity.py
@@ -0,0 +1,10 @@
+x = 5
+y = "john"
+print(x)
+print(y)
+print("codingal")
+print("welcome to the programming world")
+print(4)
+print("hello python \n")
+print("hello",5)
+print("welcome to", end="*")
