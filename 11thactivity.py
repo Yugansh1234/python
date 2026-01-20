a894cda6f7d1228a0a81b5555a4c12ee45c7545b
Yugansh1234 <yukjain1984@gmail.com>
2026-01-20 15:54:31 +0530

    Add files via upload

---
+++ 11thactivity.py
@@ -0,0 +1,14 @@
+a = 5
+b = 8
+if a>2 and b>10:
+    print("condition is true")
+else:
+    print("condition is false")
+
+a = 4
+b = 9
+if a>2 or b>10:
+    print("condition is true")
+else:
+    print("condition is false")
+    
