7ab79c1b9772e6d3ff87c1d2dba665adffdb5abf
Yugansh1234 <yukjain1984@gmail.com>
2026-01-31 16:29:36 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,10 @@
+x = int(input("enter the first number: "))
+y = int(input("enter the second number: "))
+z = int(input("enter the third number: "))
+a = x
+x = y
+y = z
+z = a
+print("value of first number is ", x)
+print("value of second number is ", y)
+print("value of third number is ", z)
